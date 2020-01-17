3d996a858cda9dae8c90536192f46dcb40c008f6
Michael R.F <mrflowers22@uwalumni.com>
2020-01-17 15:59:22 -0800

    implemented the Aircraft and Starship model objects and made them both conform to codable

---
+++ AwakenAPI/Models/Aircraft.swift
@@ -0,0 +1,13 @@
+//
+//  Aircraft.swift
+//  AwakenAPI
+//
+//  Created by Michael Flowers on 1/17/20.
+//  Copyright © 2020 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+
+struct Aircraft: Codable {
+    let results: [Starship]
+}
---
+++ AwakenAPI/Models/Starship.swift
@@ -0,0 +1,19 @@
+//
+//  Starship.swift
+//  AwakenAPI
+//
+//  Created by Michael Flowers on 1/17/20.
+//  Copyright © 2020 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+
+struct Starship: Codable {
+    let name:  String
+       let model: String
+       let costInCredits: String
+       let length: String
+       let vehicleClass: String
+       let crew: String
+    
+}
