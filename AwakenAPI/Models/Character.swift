b5853b87d3ed7a33e1fd1cdde3fb5ae1fd78f437
Michael R.F <mrflowers22@uwalumni.com>
2020-01-17 08:37:02 -0800

    deleted the people struct file and refactored the character file - got ride of the custom codable init.

---
+++ AwakenAPI/Models/Character.swift
@@ -0,0 +1,59 @@
+//
+//  Character.swift
+//  AwakenAPI
+//
+//  Created by Michael Flowers on 1/17/20.
+//  Copyright © 2020 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+
+struct Character: Codable {
+    let name: String
+    let height: Int
+    
+    /*
+     The following are representations of snake_case on the api. Here we have to be sure to capitalize the word after the "_" so that the compiler will know its the begining of the second word.
+     
+     -- We dont have to use a customized init decoder method for snake_case if we make the decoder keyDecodingStrategy =  .convertFromSnakeCase
+     */
+    
+    let birthYear: String
+    let eyeColor: String
+    let hairColor: String
+    
+      /*
+     because the api indicates the path is people, we can implement a static representation of that on the type
+     */
+    static let path = "people"
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
