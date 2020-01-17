82808ae556cdd30e144e6c760226d0bd0e023998
Michael R.F <mrflowers22@uwalumni.com>
2020-01-17 10:59:30 -0800

    implemented Transportation and vehicle structs

---
+++ AwakenAPI/Models/Transportation.swift
@@ -0,0 +1,13 @@
+//
+//  Transportation.swift
+//  AwakenAPI
+//
+//  Created by Michael Flowers on 1/17/20.
+//  Copyright © 2020 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+
+struct Transportation: Codable {
+    let results: [Vehicle]
+}
---
+++ AwakenAPI/Models/Vehicle.swift
@@ -0,0 +1,18 @@
+//
+//  Vehicle.swift
+//  AwakenAPI
+//
+//  Created by Michael Flowers on 1/17/20.
+//  Copyright © 2020 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+
+struct Vehicle: Codable {
+    let name:  String
+    let model: String
+    let cost: String
+    let length: String
+    let vehicleClass: String
+    let crew: String
+}
